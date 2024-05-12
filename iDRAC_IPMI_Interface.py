f5306c755d288a754478fddde3a3c605cb6598a6
Ben Ward <ben.g.ward@gmail.com>
2024-05-12 17:13:40 -0500

    IPMI Interface updates. Converted from a collection of functions to a class. Added using the logger instead of print. Added optimization to not re-poll for the temperature table if within a certain number of seconds.

--- iDRAC_IPMI_Interface.py
+++ iDRAC_IPMI_Interface.py
@@ -1,80 +1,90 @@
+import logging
 import subprocess
+import sys
+import time
 
-def SetFanPercent(percentage):
-    subprocess.run(["ipmitool", "raw", "0x30", "0x30", "0x02", "0xFF", hex(int(percentage))], stdout=subprocess.PIPE)
+STALE_DATA_TIMEOUT_SECONDS = 1
 
-def SetManual():
-    print("Setting to manual control.")
-    subprocess.run(["ipmitool", "raw", "0x30", "0x30", "0x01", "0x00"], stdout=subprocess.PIPE)
+class IpmiInterface:
+    def __init__(self):
+        self.automaticMode = True
+        self.logger = logging.getLogger(__name__)
+        self.temperatureTable = ""
+        self.temperatureTableTime = 0.0
 
-def SetAutomatic():
-    print("Setting to automatic control.")
-    subprocess.run(["ipmitool", "raw", "0x30", "0x30", "0x01", "0x01"], stdout=subprocess.PIPE)
+    def SetFanPercent(self,percentage):
+        if self.automaticMode:
+            self.SetManual()
 
-def GetProcessor1Temperature(temperatureTable = None):
-    if temperatureTable is None:
-        temperatureTable = GetTemperatureTable()
+        subprocess.run(["ipmitool", "raw", "0x30", "0x30", "0x02", "0xFF", hex(int(percentage))], stdout=subprocess.PIPE)
 
-    return GetTemperature("0Eh", temperatureTable)
+    def SetManual(self):
+        self.logger.info("Setting to manual control.")
+        self.automaticMode = False
+        subprocess.run(["ipmitool", "raw", "0x30", "0x30", "0x01", "0x00"], stdout=subprocess.PIPE)
 
-def GetProcessor2Temperature(temperatureTable = None):
-    if temperatureTable is None:
-        temperatureTable = GetTemperatureTable()
+    def SetAutomatic(self):
+        self.logger.info("Setting to automatic control.")
+        self.automaticMode = True
+        subprocess.run(["ipmitool", "raw", "0x30", "0x30", "0x01", "0x01"], stdout=subprocess.PIPE)
 
-    return GetTemperature("0Fh", temperatureTable)
+    def GetProcessor1Temperature(self):
+        return self.GetTemperature("0Eh")
 
-def GetInletTemperature(temperatureTable = None):
-    if temperatureTable is None:
-        temperatureTable = GetTemperatureTable()
+    def GetProcessor2Temperature(self):
+        return self.GetTemperature("0Fh")
 
-    return GetTemperature("04h", temperatureTable)
+    def GetInletTemperature(self):
+        return self.GetTemperature("04h")
 
-def GetExhaustTemperature(temperatureTable = None):
-    if temperatureTable is None:
-        temperatureTable = GetTemperatureTable()
+    def GetExhaustTemperature(self):
+        return self.GetTemperature("01h")
 
-    return GetTemperature("01h", temperatureTable)
+    def GetMaximumProcessorTemperature(self):
+        return max(self.GetProcessor1Temperature(), self.GetProcessor2Temperature())
 
-def GetMaximumProcessorTemperature():
-    table = GetTemperatureTable()
-    return max(GetProcessor1Temperature(table), GetProcessor2Temperature(table))
+    def GetTemperature(self, identifier):
+        temperature = 0
 
-def GetTemperature(identifier, temperatureTable):
-    temperature = 0
+        for line in self.GetTemperatureTable().splitlines():
+            if identifier in line:
+                temperature = int(line.split("|")[-1].strip("degrees C"))
 
-    for line in temperatureTable.splitlines():
-        if identifier in line:
-            temperature = int(line.split("|")[-1].strip("degrees C"))
+        return temperature
 
-    return temperature
+    def GetTemperatureTable(self):
+        if (time.time_ns() - self.temperatureTableTime) > (STALE_DATA_TIMEOUT_SECONDS * 1000000000.0):
+            self.logger.debug("Polling new temperature table.")
+            self.temperatureTable = subprocess.run(["ipmitool", "sdr", "type", "temperature"], stdout=subprocess.PIPE).stdout.decode()
+            self.temperatureTableTime = time.time_ns()
+        
+        return self.temperatureTable
 
-def GetTemperatureTable():
-    return subprocess.run(["ipmitool", "sdr", "type", "temperature"], stdout=subprocess.PIPE).stdout.decode()
+    def GetAverageFanRPM(self):
+        count = 0
+        sum = 0
 
-def GetAverageFanRPM():
-    count = 0
-    sum = 0
+        for line in self.GetFanRPMTableLines():
+            count = count + 1
+            rpm = int(line.split("|")[-1].strip("RPM"))
+            sum = sum + rpm
 
-    for line in GetFanRPMTableLines():
-        count = count + 1
-        rpm = int(line.split("|")[-1].strip("RPM"))
-        sum = sum + rpm
+        return sum / count
 
-    return sum / count
-
-def GetFanRPMTableLines():
-    table = subprocess.run(["ipmitool", "sdr", "type", "fan"], stdout=subprocess.PIPE).stdout.decode()
-    return [line for line in table.splitlines() if "RPM" in line ]
-
+    def GetFanRPMTableLines(self):
+        table = subprocess.run(["ipmitool", "sdr", "type", "fan"], stdout=subprocess.PIPE).stdout.decode()
+        return [line for line in table.splitlines() if "RPM" in line ]
 
 if __name__ == '__main__':
-    print(GetTemperatureTable())
-    print(f"Processor 1: {GetProcessor1Temperature()}")
-    print(f"Processor 2: {GetProcessor2Temperature()}")
-    print(f"Inlet: {GetInletTemperature()}")
-    print(f"Exhaust: {GetExhaustTemperature()}")
-    print(f"Max Processor: {GetMaximumProcessorTemperature()}")
-    print(f"Fan RPM: {GetAverageFanRPM()}")
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    impi = IpmiInterface()
+    print(impi.GetTemperatureTable())
+    print(f"Processor 1: {impi.GetProcessor1Temperature()}")
+    print(f"Processor 2: {impi.GetProcessor2Temperature()}")
+    print(f"Inlet: {impi.GetInletTemperature()}")
+    print(f"Exhaust: {impi.GetExhaustTemperature()}")
+    print(f"Max Processor: {impi.GetMaximumProcessorTemperature()}")
+    print(f"Fan RPM: {impi.GetAverageFanRPM()}")
 
     choice = "b"
 
@@ -82,9 +92,9 @@ if __name__ == '__main__':
         choice = input("[A]uto, [M]anual, [P]ercent [E]xit: ").upper()[0]
 
         if choice == "A":
-            SetAutomatic()
+            impi.SetAutomatic()
         elif choice == "M":
-            SetManual()
+            impi.SetManual()
         elif choice == "P":
             percent = input("Enter Percent: ")
-            SetFanPercent(float(percent))
+            impi.SetFanPercent(float(percent))
